fdd04d29b8c03b3c70ca58a6b12ff65837c4c906
JACK-OSULLIVAN <141579241+JACK-OSULLIVAN@users.noreply.github.com>
2023-08-24 08:32:39 +0100

    Update number_game_class_list.py

--- NumberGame/number_game_class_list.py
+++ NumberGame/number_game_class_list.py
@@ -6,15 +6,6 @@ FINAL_LIST = []
 
 USER_LIST = [9, 10, 9, 1, 50, 25]
 
-class Number:
-    def __init__(self, input_tuple, depth):
-        self.input_tuple = input_tuple
-        self.depth = depth
-        self.used_numbers = []
-
-    def create_new_number(self, depth):
-        new_depth = depth
-        pass
 
 
 class NumberGame:
@@ -36,3 +27,48 @@ class NumberGame:
         print(number_list[20].input_tuple)
 
 number_game = NumberGame(USER_LIST)
+
+
+class TupleCombination:
+    def __init__(self, tuplecombination=()):
+        self.tuple_combination = tuplecombination
+
+    @property
+    def tuple_combination(self):
+        # print("Getting value...")
+        return self._tuple_combination
+
+    @tuple_combination.setter
+    def tuple_combination(self, value):
+        # print("Setting value...")
+        if not isinstance(value, tuple):
+            raise ValueError("Requires Tuple")
+        self._tuple_combination = value
+
+
+
+user_list = [9, 10, 9, 1, 50, 25]
+
+# So first we need a function that generates all possible combinations into Tuples
+
+next_list = []
+for i in range(1, len(user_list) + 1):
+    possible_combs = list(itertools.combinations(user_list, i))
+    next_list.extend(possible_combs)
+
+print(next_list)
+
+# Assume these Tuples are similar to the numbers for Celsius, Pass in tuples to create new object
+
+object_list = []
+
+for t in next_list:
+    tuple_object = TupleCombination(t)
+    object_list.append(tuple_object)
+
+print(object_list)
+
+number_combinations = [tuple_object.tuple_combination for tuple_object in object_list]
+
+print(number_combinations)
+print(type(number_combinations[6][1]))
